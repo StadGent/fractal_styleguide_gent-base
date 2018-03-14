56e0b314ba5bf4bd4ea7c75f575745715f31ce4b
bart <bart.delrue@digipolis.gent>
2018-03-14 13:28:01 +0100

    DTGB-203: Moved fractal configuration to an imported file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 'use strict';
-
-const packageInfo = require('./package.json');
-const packageVersion = packageInfo.version;
-
 /*
 * Node core modules.
 */
 const fs = require('fs');
-const path = require('path');
 
 /*
 * NPM based modules
@@ -30,69 +25,10 @@ const inject = require('gulp-inject');
 const yargs = require('yargs');
 const combiner = require('stream-combiner2');
 const cache = require('gulp-cached');
-
-/*
-* Require the Fractal module
-*/
-const fractal = require('@frctl/fractal').create();
-const logger = fractal.cli.console; // keep a reference to the fractal CLI
-                                    // console utility
-
-/**
- * Require additional fractal modules
- */
-const twigAdapter = require('@frctl/twig');
-
-/*
-* Give your project a title.
-*/
-fractal.set('project.title', 'City of Ghent Style Guide - Version ' + packageVersion);
-
-/*
-* Tell Fractal where to look for components.
-*/
-fractal.components.set('path', path.join(__dirname, 'components'));
-fractal.components.set('default.preview', '@preview');
-fractal.components.engine(twigAdapter);
-fractal.components.set('ext', '.twig');
-
-/*
-* Set custom statuses.
-*/
-fractal.components.set('statuses', {
-  deprecated: {
-    label: 'deprecated',
-    description: 'Deprecated.',
-    color: '#dd5e01'
-  },
-  alpha: {
-    label: 'alpha',
-    description: 'Alpha software can be unstable and could cause crashes or data loss.',
-    color: '#551A8B'
-  },
-  beta: {
-    label: 'beta',
-    description: 'Work in progress. Implement with caution.',
-    color: '#ff9233'
-  },
-  ready: {
-    label: 'Ready',
-    description: 'Ready to implement.',
-    color: '#29cc29'
-  }
-});
-
-/*
-* Tell Fractal where to look for documentation pages.
-*/
-fractal.docs.set('path', path.join(__dirname, 'docs'));
-
-/*
-* Tell the Fractal web preview plugin where to look for static assets.
-*/
-fractal.web.set('static.path', path.join(__dirname, 'public'));
-fractal.web.set('static.mount', '');
-fractal.web.set('builder.dest', __dirname + '/build');
+// require our configurated fractal module
+const fractal = require('./fractal');
+// keep a reference to the fractal CLI console utility
+const logger = fractal.cli.console;
 
 /*
 * Get the sassFiles.
