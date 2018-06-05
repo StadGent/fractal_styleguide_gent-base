2e5989333794a58877ca62a5b8097d565af713db
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-06-05 15:42:14 +0200

    DTGB-209: Fixes after feedback.

--- components/21-atoms/inputs/input-multiselect-tagging/input-multiselect-tagging.bindings.js
+++ components/21-atoms/inputs/input-multiselect-tagging/input-multiselect-tagging.bindings.js
@@ -1,14 +1,18 @@
 /**
  * @file
- * Javascript binding of baguettebox.min.js.
+ * Javascript binding of choices.min.js.
  */
 'use strict';
 
 (function () {
 
+  if (!Choices) { // eslint-disable-line no-undef
+    return;
+  }
+
   var elements = document.getElementsByClassName('multiselect-tagging'); // eslint-disable-line no-undef
 
-  for (var i = 0; i < elements.length; i++) {
+  for (var i = elements.length; i--;) {
     new Choices(elements[i], { // eslint-disable-line no-undef
       searchEnabled: false,
       removeItems: true,
