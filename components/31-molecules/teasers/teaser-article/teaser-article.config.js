386c61bdfc9dfdaca9e6c19a6fdc13e548741478
bart <bart.delrue@digipolis.gent>
2018-06-14 17:19:40 +0200

    DTGB-443: Added extra articles to news page.

--- components/31-molecules/teasers/teaser-article/teaser-article.config.js
+++ components/31-molecules/teasers/teaser-article/teaser-article.config.js
@@ -4,14 +4,32 @@ module.exports = {
   title: 'Article teaser',
   status: 'ready',
   name: 'teaser-article',
+  hidden: true,
   context: {
     teaser_uid: 'uid',
     teaser_paragraph_text: 'Buitenschoolse opvang De Rakkertjes en Basisschool Victor Carpentier organiseren een vervolg op de LEF-actie van vorig jaar en…',
-    teaser_h3_title: 'Kom naar de paasspeurtocht en Shop-op Zondag op 1 april',
-    teaser_event_date: 'Zaterdag 7 januari 2019',
+    teaser_title: 'Kom naar de paasspeurtocht en Shop-op Zondag op 1 april',
+    teaser_published_date: 'Gepost op Zaterdag 7 januari 2019',
     teaser_link_text: 'read more',
-    teaser_link_link: 'javascript:void(0);',
-    teaser_image_src: 'https://loremflickr.com/1360/800/ghent',
-    teaser_image_alt_text: 'alt text'
-  }
+    teaser_link_link: '#',
+    teaser_image_src: 'https://via.placeholder.com/800x500',
+    teaser_image_alt_text: 'alt text',
+    teaser_tag: 'Mobiliteit & openbare werken'
+  },
+  variants: [
+    {
+      name: 'event',
+      context: {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Buitenschoolse opvang De Rakkertjes en Basisschool Victor Carpentier organiseren een vervolg op de LEF-actie van vorig jaar en…',
+        teaser_title: 'Kom naar de paasspeurtocht en Shop-op Zondag op 1 april',
+        teaser_event_date: 'Zaterdag 7 januari 2019',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/800x500',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Mobiliteit & openbare werken'
+      }
+    }
+  ]
 };
