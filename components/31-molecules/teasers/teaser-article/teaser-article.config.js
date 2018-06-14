9b5735ecb596ecb6eb4596e7e6543d818647ae2d
bart <bart.delrue@digipolis.gent>
2018-06-14 15:49:54 +0200

    DTGB-443: Styled article teaser.

---
+++ components/31-molecules/teasers/teaser-article/teaser-article.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  title: 'Article teaser',
+  status: 'ready',
+  name: 'teaser-article',
+  context: {
+    teaser_uid: 'uid',
+    teaser_paragraph_text: 'Buitenschoolse opvang De Rakkertjes en Basisschool Victor Carpentier organiseren een vervolg op de LEF-actie van vorig jaar en…',
+    teaser_h3_title: 'Kom naar de paasspeurtocht en Shop-op Zondag op 1 april',
+    teaser_event_date: 'Zaterdag 7 januari 2019',
+    teaser_link_text: 'read more',
+    teaser_link_link: 'javascript:void(0);',
+    teaser_image_src: 'https://loremflickr.com/1360/800/ghent',
+    teaser_image_alt_text: 'alt text'
+  }
+};
---
+++ components/31-molecules/teasers/teaser-topic/teaser-topic.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  title: 'Topic teaser',
+  status: 'ready',
+  name: 'teaser-topic',
+  context: {
+    teaser_uid: 'uid',
+    teaser_paragraph_text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, suscipit.',
+    teaser_h3_title: 'Title text',
+    teaser_link_text: 'read more',
+    teaser_link_link: 'javascript:void(0);',
+    teaser_image_src: 'https://loremflickr.com/1360/800/ghent',
+    teaser_image_alt_text: 'alt text'
+  }
+};
