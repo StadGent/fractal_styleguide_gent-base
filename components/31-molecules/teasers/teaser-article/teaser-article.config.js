0d5e110b3a04a75746bc7eb647bfeb056891f995
bart <bart.delrue@digipolis.gent>
2018-06-18 11:12:45 +0200

    DTGB-450: Refactored event teaser date component.

--- components/31-molecules/teasers/teaser-article/teaser-article.config.js
+++ components/31-molecules/teasers/teaser-article/teaser-article.config.js
@@ -23,8 +23,12 @@ module.exports = {
         teaser_uid: 'uid',
         teaser_paragraph_text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corporis cumque debitis eligendi harum iste laborum quibusdam ut, vitae voluptate?',
         teaser_title: 'News article title text',
-        teaser_event_date: 'Saturday 7th of January 2019',
-        teaser_datetime: '2019-01-07',
+        teaser_event_date: {
+          text_1: 'From the 7th',
+          datetime_1: '2019-01-07',
+          text_2: 'untill the 13th of January 2019',
+          datetime_2: '2019-01-13'
+        },
         teaser_link_text: 'read more',
         teaser_link_link: '#',
         teaser_image_src: 'https://via.placeholder.com/800x500',
