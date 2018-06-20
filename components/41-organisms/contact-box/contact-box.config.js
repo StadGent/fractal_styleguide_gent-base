856b6a273e2b6a46f5188df5b87d31917ad3f204
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-06-20 10:59:23 +0200

    DTGB-452: Added first draft for contact box.

---
+++ components/41-organisms/contact-box/contact-box.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  title: 'Contact box',
+  status: 'alpha',
+  context: {
+    contacts: [
+      {
+        link: '#',
+        link_text: 'Thomas Vandersteen',
+        address: 'Kasteelwegel 23<br />9000 Gent',
+        email: 'thomas.vandersteen@stad.gent'
+      },
+      {
+        link: '#',
+        link_text: 'Tester De Test',
+        address: 'Kouter 23<br />9000 Gent',
+        email: 'tester.de.test@stad.gent'
+      }
+    ],
+    other_contacts: [
+      {
+        link: '#',
+        link_text: 'Thomas Vandersteen'
+      },
+      {
+        link: '#',
+        link_text: 'Tester De Test'
+      }
+    ]
+  }
+};
