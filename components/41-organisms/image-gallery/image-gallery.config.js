c906a2f78a89809767dc9fa13bb4a4eb2612f699
bart <bart.delrue@digipolis.gent>
2018-06-26 16:50:13 +0200

    DTGB-minors: Collated image-gallery.

---
+++ components/41-organisms/image-gallery/image-gallery.config.js
@@ -0,0 +1,173 @@
+'use strict';
+
+module.exports = {
+  title: 'Image gallery',
+  status: 'ready',
+  name: 'Image gallery',
+  preview: '@preview-description-list',
+  collated: true,
+  collator: function (markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<dt>${item.label || item.name}</dt><dd>${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
+  },
+  variants: [
+    {
+      name:'multiple',
+      hidden: true
+    },
+    {
+      name: 'default',
+      label: 'Single',
+      preview: '@preview',
+      context: {
+        src: 'https://via.placeholder.com/704x440',
+        alt: 'placeholder image alternative text',
+        caption: 'image caption'
+      }
+    },
+    {
+      name: 'double',
+      view: 'image-gallery--multiple',
+      preview: '@preview',
+      context: {
+        items: [
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text'
+          }
+        ]
+      }
+    },
+    {
+      name: 'triple',
+      view: 'image-gallery--multiple',
+      preview: '@preview',
+      context: {
+        items: [
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text'
+          }
+        ]
+      }
+    },
+    {
+      name: 'quadruple',
+      view: 'image-gallery--multiple',
+      preview: '@preview',
+      context: {
+        items: [
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text'
+          }
+        ]
+      }
+    },
+    {
+      name: 'quintuple',
+      view: 'image-gallery--multiple',
+      preview: '@preview',
+      context: {
+        items: [
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text'
+          }
+        ]
+      }
+    },
+    {
+      name: 'quintuple et plus',
+      view: 'image-gallery--multiple',
+      preview: '@preview',
+      context: {
+        items: [
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text',
+            caption: 'image caption'
+          },
+          {
+            src: 'https://via.placeholder.com/704x440',
+            alt: 'placeholder image alternative text'
+          }
+        ]
+      }
+    }
+  ]
+};
