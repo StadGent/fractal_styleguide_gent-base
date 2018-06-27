77e4b09b2665e85a04ed9c43c7afe53403eb71b5
bart <bart.delrue@digipolis.gent>
2018-06-27 21:08:22 +0200

    DTGB-minors: Added rgb and rgba values to colors preview.

---
+++ components/11-base/colors/colors.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  title: 'Colors',
+  status: 'ready',
+  handle: 'colors',
+  context: {
+    primary: {
+      'cyan': '#009de0',
+      'cyan-wcag': '#007db3',
+      'dark-gray': '#23333a',
+      'white': '#ffffff'
+    },
+    secondary: {
+      blue: '#0340c7',
+      teal: '#29cfc9',
+      green: '#38ab30',
+      lemon: '#ffda00',
+      yellow: '#fab600',
+      orange: '#f95706',
+      red: '#f20f36',
+      pink: '#ff0099',
+      purple: '#5a0ec4'
+    },
+    tints: {
+      '1': '10%',
+      '2': '25%',
+      '3': '50%',
+      '-1': '75%',
+      '-2': '50%',
+      '-3': '25%',
+      '-4': '10%',
+      '-5': '6%',
+      '-6': '2%'
+    }
+  }
+};
