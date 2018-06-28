3fc913c7ff0ebd62c7a9b4cfdc63454f9ba74bf1
Jeroen Goossens <jeroen@pau.be>
2018-06-28 14:22:19 +0200

    DTGB-461: Fixed pr feedback.

--- components/21-atoms/heading/README.md
+++ components/21-atoms/heading/README.md
@@ -14,7 +14,18 @@ They provide a quick way to scan the main sections of a website.
 * Don’t use links in page headings.
 * Use only one H1 per page.
 * Don’t skip headings: e.g. do not jump from H1 to H3.
-* Currently, only Headings 1 to 3 are used.
+
+## Variants
+There a few title variants available in style guide.
+
+- **Overview titles:** Can be used above an overview, mostly as the page title.
+  This variant is always styled as a heading 1.
+- **Collection titles:** Can be used above a collection of teasers or tiles.
+  This variant is always styled as a heading 2.
+
+Simply add the title class on any heading element (according SEO guidelines) to
+apply this style. It isn't required to use these classes on the default heading
+element provided.
 
 ## Web accessibility
 
--- components/21-atoms/heading/heading.config.js
+++ components/21-atoms/heading/heading.config.js
@@ -1,16 +1,48 @@
 'use strict';
 
 module.exports = {
-  title: 'Titles',
+  title: 'Headings',
   status: 'alpha',
-  name: 'titles',
+  name: 'heading',
   collated: true,
   collator: function (markup, item) {
     return `<!-- Start: @${item.handle} -->\n<dt>${item.name}</dt><dd style="max-width: 100%">${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
   },
   preview: '@preview-description-list',
-  default: 'overview-title',
+  default: 'heading-1',
   variants: [
+    {
+      name: 'heading-1',
+      preview: '@preview',
+      context: {
+        heading_text: 'Heading 1',
+        type: 'h1'
+      }
+    },
+    {
+      name: 'heading-2',
+      preview: '@preview',
+      context: {
+        heading_text: 'Heading 2',
+        type: 'h2'
+      }
+    },
+    {
+      name: 'heading-3',
+      preview: '@preview',
+      context: {
+        heading_text: 'Heading 3',
+        type: 'h3'
+      }
+    },
+    {
+      name: 'heading-4',
+      preview: '@preview',
+      context: {
+        heading_text: 'Heading 4',
+        type: 'h4'
+      }
+    },
     {
       name: 'overview-title',
       preview: '@preview',
