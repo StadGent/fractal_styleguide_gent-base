65fef0b446bfe66a520df043b1f5a4a8d737d801
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-08-02 09:03:44 +0200

    DTGB-488: Added summary box instead of info box + base accolade support.

---
+++ components/21-atoms/accolade/accolade.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  title: 'Accolade',
+  hidden: 'hidden',
+  status: 'ready',
+  handle: 'accolade'
+};
---
+++ components/41-organisms/summary-box/summary-box.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  title: 'Summary box',
+  status: 'alpha',
+  context: {
+    tags: [
+      'Inner city',
+      'Another tag'
+    ]
+  }
+};
