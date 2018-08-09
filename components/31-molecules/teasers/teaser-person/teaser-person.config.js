e6b0c64a57213772853ef49b2235de7c2cd85ee8
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-08-09 11:16:30 +0200

    DTGB-496: Added person teasers and overviews + sidebar layout.

---
+++ components/31-molecules/teasers/teaser-person/teaser-person.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  title: 'Person teaser',
+  status: 'ready',
+  name: 'teaser-person',
+  hidden: true,
+  context: {
+    teaser_uid: 'uid',
+    teaser_paragraph_text: 'Function or job description comes here',
+    teaser_title: 'Name and first name',
+    teaser_link_text: 'read more',
+    teaser_link_link: '#',
+    teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+    teaser_image_alt_text: 'alt text',
+    teaser_tag: 'Just some tag'
+  }
+};
---
+++ components/71-pages/general/person-overview-page/person-overview-page.config.js
@@ -0,0 +1,106 @@
+'use strict';
+
+module.exports = {
+  title: 'Person Overview page',
+  status: 'alpha',
+  preview: '@preview-overview-without-padding',
+  context: {
+    people: [
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      },
+      {
+        teaser_uid: 'uid',
+        teaser_paragraph_text: 'Function or job description comes here',
+        teaser_title: 'Name and first name',
+        teaser_link_text: 'read more',
+        teaser_link_link: '#',
+        teaser_image_src: 'https://via.placeholder.com/280x280&text=1:1+(280x280)',
+        teaser_image_alt_text: 'alt text',
+        teaser_tag: 'Just some tag'
+      }
+    ]
+  },
+  variants: [
+    {
+      name: 'with-filters'
+    }
+  ]
+};
