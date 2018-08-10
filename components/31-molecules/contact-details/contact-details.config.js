fca6a1d26903440cb1cd265f1777208c4ee3edc9
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-08-10 14:42:24 +0200

    DTGB-501: Added person detail page setup.

---
+++ components/31-molecules/contact-details/contact-details.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  title: 'CTA block',
+  status: 'alpha',
+  handle: 'cta-block',
+  context: {
+    title: 'This is a title',
+    description: 'Nulla nec eros lorem. Morbi at augue eu purus congue auctor. Sed id volutpat odio. Nunc congue diam vel diam venenatis, ut consequat urna dapibus. Vivamus at lobortis odio. Sed felis mauris, mattis eget purus at, pharetra ullamcorper risus.',
+    type: 'link',
+    links: [
+      {
+        text: 'In the summertime',
+        url: '#',
+        document: 'document',
+        size: '102kb'
+      },
+      {
+        text: 'When the weather is hot',
+        url: '#'
+      }
+    ]
+  }
+};
