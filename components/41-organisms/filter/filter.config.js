487a903f9b0765a1814326a966948e701064a4ce
bart <bart.delrue@digipolis.gent>
2018-08-17 09:38:43 +0200

    DTGB-509: Added modal functions and filter-modal bindings.

--- components/41-organisms/filter/filter.config.js
+++ components/41-organisms/filter/filter.config.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const generateCheckboxes = (uid)=>{
+  let result = [];
+  for (let i = 195; i--;) {
+    result.push({
+      label_checkbox: `Checkbox option ${i}`,
+      checkbox_name: 'checkboxgroup[]',
+      checkbox_id: `checkbox-${i}-${uid}`,
+      checkbox_value: i
+    });
+  }
+  return result;
+};
+
 module.exports = {
   title: 'Filter',
   status: 'alpha',
   preview: '@preview-without-padding',
-  name: 'Filter'
+  name: 'Filter',
+  context: {
+    checkboxes: generateCheckboxes('checkbox-with-filter')
+  }
 };
