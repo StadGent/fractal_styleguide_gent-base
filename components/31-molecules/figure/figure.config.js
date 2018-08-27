cae9938b59f5e96b6c9f046689b178eb4d16de5e
Jeroen Goossens <jeroen@pau.be>
2018-08-27 11:55:30 +0200

    DTGB-510: Refactored in placeholders. Now default available in styleguide.

--- components/31-molecules/figure/figure.config.js
+++ components/31-molecules/figure/figure.config.js
@@ -10,7 +10,7 @@ module.exports = {
     figure_figcaption: 'Figure caption comes here.',
     figure_image_src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
     figure_image_alt_text: 'Image alt text',
-    figure_ratio: '8:5',
+    figure_ratio: '8:5'
   },
   variants: [
     {
