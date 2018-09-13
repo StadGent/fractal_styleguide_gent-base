95d61033d13931130428537848778be28739cda4
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-09-13 11:10:07 +0200

    DTGB-452: Changes to the contact box.

--- components/41-organisms/contact-box/contact-box.config.js
+++ components/41-organisms/contact-box/contact-box.config.js
@@ -5,29 +5,29 @@ module.exports = {
   status: 'alpha',
   context: {
     unique_id: '1',
+    contact_categories: [
+      {
+        link: '#',
+        name: 'Contact category name',
+        description: 'eros mauris. Vestibulum gravida efficitur ligula, quis hendrerit est euismod ac'
+      }
+    ],
     contacts: [
       {
         link: '#',
         link_text: 'Thomas Vandersteen',
         address: 'Kasteelwegel 23<br />9000 Gent',
+        phone: '09 123 45 67',
         email: 'thomas.vandersteen@stad.gent',
         opening_hours: 'From 10:00 till 18:00'
       },
       {
         link: '#',
-        link_text: 'Tester De Test',
-        address: 'Kouter 23<br />9000 Gent',
-        email: 'tester.de.test@stad.gent'
-      }
-    ],
-    other_contacts: [
-      {
-        link: '#',
-        link_text: 'Thomas Vandersteen'
-      },
-      {
-        link: '#',
-        link_text: 'Tester De Test'
+        link_text: 'Thomas Vandersteen',
+        address: 'Kasteelwegel 23<br />9000 Gent',
+        phone: '09 123 45 67',
+        email: 'thomas.vandersteen@stad.gent',
+        opening_hours: 'From 10:00 till 18:00'
       }
     ],
     authorized_persons: [
