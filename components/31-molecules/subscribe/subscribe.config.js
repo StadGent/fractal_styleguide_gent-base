e9c68329043046e898701732b34f894f95465069
bart <bart.delrue@digipolis.gent>
2018-09-20 14:47:04 +0200

    DTGB-564: Added subscribe molecule.

---
+++ components/31-molecules/subscribe/subscribe.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  name: 'Subscribe',
+  status: 'beta',
+  hidden: true,
+  context: {
+    label: 'Email address',
+    text: 'Subscribe',
+    id: 'subscribe',
+    name: 'subscribe'
+  }
+};
