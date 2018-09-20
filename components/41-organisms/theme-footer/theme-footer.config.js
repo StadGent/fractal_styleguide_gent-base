2d602946ce6be07a8d98eba5134d8c781ecce927
bart <bart.delrue@digipolis.gent>
2018-09-20 14:47:41 +0200

    DTGB-564: Added theme footer.

--- components/41-organisms/theme-footer/theme-footer.config.js
+++ components/41-organisms/theme-footer/theme-footer.config.js
@@ -3,7 +3,130 @@
 module.exports = {
   name: 'Theme footer',
   handle: 'theme-footer',
-  hidden: 'hidden',
-  status: 'alpha',
-  preview: '@preview-without-padding'
+  status: 'beta',
+  preview: '@preview-description-list-without-padding',
+  collated: true,
+  collator: function (markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<dt>${item.name}</dt><dd style="max-width: 100%; padding: 0;">${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
+  },
+  default: 'contact-newsletter-social',
+  variants: [
+    {
+      name: 'contact-newsletter-social',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        contact: {
+          address: {
+            name: 'City hall',
+            address: 'Botermarkt 1, 9000 Gent'
+          },
+          phone: {
+            machine: '+3292101060',
+            human: '09 210 10 60'
+          },
+          email: 'noreply@stad.gent',
+          form: {
+            machine: '#',
+            human: 'Contactform Ghent mobility'
+          }
+        },
+        newsletter: {},
+        social: {}
+      }
+    },
+    {
+      name: 'contact',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        contact: {
+          address: {
+            name: 'City hall',
+            address: 'Botermarkt 1, 9000 Gent'
+          },
+          phone: {
+            machine: '+3292101060',
+            human: '09 210 10 60'
+          },
+          email: 'noreply@stad.gent',
+          form: {
+            machine: '#',
+            human: 'Contactform Ghent mobility'
+          }
+        }
+      }
+    },
+    {
+      name: 'newsletter',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        newsletter: {}
+      }
+    },
+    {
+      name: 'social',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        social: {}
+      }
+    },
+    {
+      name: 'contact-newsletter',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        contact: {
+          address: {
+            name: 'City hall',
+            address: 'Botermarkt 1, 9000 Gent'
+          },
+          phone: {
+            machine: '+3292101060',
+            human: '09 210 10 60'
+          },
+          email: 'noreply@stad.gent',
+          form: {
+            machine: '#',
+            human: 'Contactform Ghent mobility'
+          }
+        },
+        newsletter: {}
+      }
+    },
+    {
+      name: 'contact-social',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        contact: {
+          address: {
+            name: 'City hall',
+            address: 'Botermarkt 1, 9000 Gent'
+          },
+          phone: {
+            machine: '+3292101060',
+            human: '09 210 10 60'
+          },
+          email: 'noreply@stad.gent',
+          form: {
+            machine: '#',
+            human: 'Contactform Ghent mobility'
+          }
+        },
+        social: {}
+      }
+    },
+    {
+      name: 'newsletter-social',
+      preview: '@preview',
+      context: {
+        theme: 'Ghent mobility',
+        newsletter: {},
+        social: {}
+      }
+    }
+  ]
 };
