0952331d5017f992d2b92677adfd9b0cf0b39e57
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-10-10 17:10:20 +0200

    DTGB-580: Added more styling to the teaser.

---
+++ components/31-molecules/teasers/teaser-search-wide/teaser-search-wide.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  title: 'Search teaser wide',
+  status: 'ready',
+  name: 'teaser-search-wide',
+  hidden: true,
+  context: {
+    teaser_uid: 'uid',
+    teaser_paragraph_text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corporis cumque debitis eligendi harum iste laborum quibusdam ut, vitae voluptate?',
+    teaser_title: 'General article title text',
+    teaser_link_text: 'read more',
+    teaser_link_link: '#',
+    teaser_image_src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
+    teaser_image_alt_text: 'alt text',
+    teaser_tag: 'Just some tag'
+  }
+};
