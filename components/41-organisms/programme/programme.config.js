0a4c31d89307a7dda3b864d5f0b8af6fef3b53e1
hstandaert <helena.standaert@gmail.com>
2018-10-12 16:09:14 +0200

    DTGB-555: Add overview markup for programme organism

---
+++ components/41-organisms/programme/programme.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  title: 'Programme',
+  status: 'alpha',
+  preview: '@preview-without-padding',
+  name: 'Programme',
+  context: {
+    slots: [
+      {
+        title: 'Vrijdag 19 augustus 2018',
+        items: [
+          {
+            title: 'General article title text',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          },
+          {
+            title: 'Lorem ipsum dolor sit amet bacon ipsum is fun',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          },
+          {
+            title: 'General article title text',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          },
+          {
+            title: 'Lorem ipsum dolor sit amet',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          }
+        ]
+      },
+      {
+        title: 'Zaterdag 20 augustus 2018',
+        items: [
+          {
+            title: 'General article title text',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          },
+          {
+            title: 'General article title text',
+            subtitle: 'Ongoing animation',
+            image: 'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+          }
+        ]
+      }
+    ]
+  }
+};
