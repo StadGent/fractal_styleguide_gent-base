a775dd082892635bb412d640f7de12b7c1b9d841
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-10-16 10:07:09 +0200

    DTGB-554: Added preview fix.

--- components/31-molecules/contact-block/contact-block.config.js
+++ components/31-molecules/contact-block/contact-block.config.js
@@ -5,6 +5,7 @@ module.exports = {
   default: 'contact-block-with-phone',
   status: 'alpha',
   handle: 'contact-block',
+  preview: '@preview-description-list',
   collated: true,
   collator: function (markup, item) {
     return `<!-- Start: @${item.handle} -->\n<dt>${item.name}</dt><dd>${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
@@ -18,10 +19,12 @@ module.exports = {
   },
   variants: [
     {
-      name: 'contact-block-with-phone'
+      name: 'contact-block-with-phone',
+      preview: '@preview'
     },
     {
       name: 'contact-block-with-email',
+      preview: '@preview',
       context: {
         title: 'Contact us',
         description: 'Nulla nec eros lorem. Morbi at augue eu purus congue auctor. Sed id volutpat odio. Nunc congue diam vel diam venenatis, ut consequat urna dapibus. Vivamus at lobortis odio. Sed felis mauris, mattis eget purus at, pharetra ullamcorper risus.',
