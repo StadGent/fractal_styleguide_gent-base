eaa71267a2c991aea9435e0e0c8b6f608b7d6c5e
hstandaert <helena.standaert@gmail.com>
2018-10-16 10:41:04 +0200

    DTGB-555: DTGB-555: Added all possible content in modal

--- components/41-organisms/programme/programme.config.js
+++ components/41-organisms/programme/programme.config.js
@@ -30,7 +30,12 @@ module.exports = {
                 alt: 'placeholder image alternative text',
                 caption: 'image caption'
               }
-            ]
+            ],
+            video: {
+              title: 'Promofilm stad Gent.',
+              src: 'https://www.youtube.com/embed/S0vLmTWUmok',
+              audio_description_src: 'https://www.youtube.com/embed/S0vLmTWUmok'
+            }
           },
           {
             title: 'Lorem ipsum dolor sit amet bacon ipsum is fun',
@@ -41,11 +46,17 @@ module.exports = {
                 alt: 'placeholder image alternative text',
                 caption: 'image caption'
               }
-            ]
+            ],
+            video: {
+              title: 'Promofilm stad Gent.',
+              src: 'https://www.youtube.com/embed/S0vLmTWUmok',
+              audio_description_src: 'https://www.youtube.com/embed/S0vLmTWUmok'
+            }
           },
           {
             title: 'General article title text',
-            subtitle: 'Ongoing animation'
+            subtitle: 'Ongoing animation',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum imperdiet vestibulum ex, id tincidunt nulla porttitor nec. Cras aliquam interdum felis, nec efficitur quam varius sit amet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut nec gravida tellus, quis pulvinar enim. Proin ut lectus dui. Pellentesque maximus orci quis aliquet bibendum. Fusce vestibulum velit a tellus fermentum, in laoreet est pharetra.',
           },
           {
             title: 'Lorem ipsum dolor sit amet',
@@ -65,15 +76,26 @@ module.exports = {
           {
             title: 'General article title text',
             subtitle: 'Ongoing animation',
-            image: [
-              'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+            images: [
+              {
+                src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
+                alt: 'placeholder image alternative text',
+                caption: 'image caption'
+              },
+              {
+                src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
+                caption: 'image caption'
+              }
             ]
           },
           {
             title: 'General article title text',
             subtitle: 'Ongoing animation',
-            image: [
-              'https://via.placeholder.com/800x500&text=8:5+(800x500)'
+            images: [
+              {
+                src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
+                alt: 'placeholder image alternative text',
+              }
             ]
           }
         ]
