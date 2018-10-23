4951b6a405b31519ed3332a6f278382e19d38192
hstandaert <helena.standaert@gmail.com>
2018-10-23 13:39:39 +0200

    DTGB-555: Fixed codeclimate errors

--- components/41-organisms/programme/programme.bindings.js
+++ components/41-organisms/programme/programme.bindings.js
@@ -1,15 +1,14 @@
-/* global Modal */
 'use strict';
 
 (function () {
 
-  if (!Modal) {
+  if (!Modal) { // eslint-disable-line no-undef
     return;
   }
 
   let selected = document.querySelectorAll('.programme-detail');
   for (let i = selected.length; i--;) {
-    new Modal(selected[i], {
+    new Modal(selected[i], { // eslint-disable-line no-undef
       changeHash: true
     });
   }
