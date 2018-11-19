4b9c36f2ef134001a2580c497edab12c41c4b38c
bart <bart.delrue@digipolis.gent>
2018-11-19 16:37:08 +0100

    DTGB-634: Added CTA block variants.

--- components/31-molecules/cta-block/cta-block.config.js
+++ components/31-molecules/cta-block/cta-block.config.js
@@ -4,21 +4,145 @@ module.exports = {
   title: 'CTA block',
   status: 'alpha',
   handle: 'cta-block',
+  collated: 'true',
+  collator: function (markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<dt>${item.name}</dt><dd>${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
+  },
+  preview: '@preview-description-list',
   context: {
-    title: 'This is a title',
-    description: 'Nulla nec eros lorem. Morbi at augue eu purus congue auctor. Sed id volutpat odio. Nunc congue diam vel diam venenatis, ut consequat urna dapibus. Vivamus at lobortis odio. Sed felis mauris, mattis eget purus at, pharetra ullamcorper risus.',
     type: 'link',
-    links: [
-      {
-        text: 'In the summertime',
-        url: '#',
-        document: 'document',
-        size: '102kb'
-      },
-      {
-        text: 'When the weather is hot',
-        url: '#'
+    title: 'This is a title',
+    description: 'Nulla nec eros lorem. Morbi at augue eu purus congue auctor. Sed id volutpat odio. Nunc congue diam vel diam venenatis.'
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Single',
+      preview: '@preview',
+      context: {
+        description: false,
+        links: [
+          {
+            text: 'When the weather is hot',
+            url: '#'
+          }
+        ]
       }
-    ]
-  }
+    },
+    {
+      name: 'default with long title',
+      preview: '@preview',
+      context: {
+        description: false,
+        title: 'Dit is een titel. Liquorice chocolate ice cream cheesecake jelly beans marzipan chocolate.',
+        links: [
+          {
+            text: 'When the weather is hot',
+            url: '#'
+          }
+        ]
+      }
+    },
+    {
+      name: 'default with optional information',
+      preview: '@preview',
+      context: {
+        links: [
+          {
+            text: 'When the weather is hot',
+            url: '#'
+          }
+        ]
+      }
+    },
+    {
+      name: 'with secondary action (link)',
+      preview: '@preview',
+      context: {
+        links: [
+          {
+            text: 'In the summertime',
+            url: '#'
+          },
+          {
+            text: 'When the weather is hot',
+            url: '#'
+          }
+        ]
+      }
+    },
+    {
+      name: 'with secondary action (download)',
+      preview: '@preview',
+      context: {
+        links: [
+          {
+            text: 'In the summertime',
+            url: '#'
+          },
+          {
+            text: 'When the weather is hot',
+            url: '#',
+            document: 'document',
+            size: '102kb'
+          }
+        ]
+      }
+    },
+    {
+      name: 'download',
+      preview: '@preview',
+      context: {
+        type: 'download',
+        links: [
+          {
+            text: 'When the weather is hot',
+            url: '#',
+            document: 'document',
+            size: '102kb'
+          }
+        ]
+      }
+    },
+    {
+      name: 'download with secondary action (link)',
+      preview: '@preview',
+      context: {
+        type: 'download',
+        links: [
+          {
+            text: 'In the summertime',
+            url: '#',
+            document: 'document',
+            size: '102kb'
+          },
+          {
+            text: 'When the weather is hot',
+            url: '#'
+          }
+        ]
+      }
+    },
+    {
+      name: 'download with secondary action (download)',
+      preview: '@preview',
+      context: {
+        type: 'download',
+        links: [
+          {
+            text: 'In the summertime',
+            url: '#',
+            document: 'document',
+            size: '102kb'
+          },
+          {
+            text: 'When the weather is hot',
+            url: '#',
+            document: 'document',
+            size: '102kb'
+          }
+        ]
+      }
+    }
+  ]
 };
