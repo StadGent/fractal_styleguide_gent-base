85348643d5c6cb47cfda3d2fd993abde1eb90d0e
Helena Standaert <helena.standaert@gmail.com>
2018-11-22 09:00:11 +0100

    SGD8-259: Added variants for contact-box

--- components/41-organisms/contact-box/contact-box.config.js
+++ components/41-organisms/contact-box/contact-box.config.js
@@ -1,44 +1,88 @@
 'use strict';
 
+const data = {
+  unique_id: '1',
+  map: true,
+  contact_categories: [
+    {
+      link: '#',
+      name: 'Contact category name'
+    }
+  ],
+  contacts: [
+    {
+      link: '#',
+      link_text: 'Thomas Vandersteen',
+      address: 'Kasteelwegel 23, 9000 Gent',
+      phone: '09 123 45 67',
+      email: 'thomas.vandersteen@stad.gent',
+      opening_hours: 'From 10:00 till 18:00'
+    },
+    {
+      link: '#',
+      link_text: 'Thomas Vandersteen',
+      address: 'Kasteelwegel 23, 9000 Gent',
+      phone: '09 123 45 67',
+      email: 'thomas.vandersteen@stad.gent',
+      opening_hours: 'From 10:00 till 18:00'
+    }
+  ],
+  authorized_persons: [
+    {
+      link: '#',
+      link_text: 'Thomas Vandersteen'
+    },
+    {
+      link: '#',
+      link_text: 'Tester De Test'
+    }
+  ]
+};
+
 module.exports = {
   title: 'Contact box',
   status: 'alpha',
-  context: {
-    unique_id: '1',
-    contact_categories: [
-      {
-        link: '#',
-        name: 'Contact category name',
-        description: 'eros mauris. Vestibulum gravida efficitur ligula, quis hendrerit est euismod ac'
-      }
-    ],
-    contacts: [
-      {
-        link: '#',
-        link_text: 'Thomas Vandersteen',
-        address: 'Kasteelwegel 23, 9000 Gent',
-        phone: '09 123 45 67',
-        email: 'thomas.vandersteen@stad.gent',
-        opening_hours: 'From 10:00 till 18:00'
-      },
-      {
-        link: '#',
-        link_text: 'Thomas Vandersteen',
-        address: 'Kasteelwegel 23, 9000 Gent',
-        phone: '09 123 45 67',
-        email: 'thomas.vandersteen@stad.gent',
-        opening_hours: 'From 10:00 till 18:00'
-      }
-    ],
-    authorized_persons: [
-      {
-        link: '#',
-        link_text: 'Thomas Vandersteen'
-      },
-      {
-        link: '#',
-        link_text: 'Tester De Test'
-      }
-    ]
-  }
+  context: data,
+
+  variants: [
+    {
+      name: 'default'
+    },
+
+    {
+      name: 'no-map',
+      context: Object.assign({}, data, {
+        map: false
+      })
+    },
+
+    {
+      name: 'contacts-only',
+      context: Object.assign({}, data, {
+        authorized_persons: [],
+        map: false
+      })
+    },
+
+    {
+      name: 'compact',
+      context: (function () {
+        let dataCopy = Object.assign({}, data);
+
+        // Duplicate contacts so we have twice as much contacts
+        dataCopy.contacts = dataCopy.contacts
+          .map(function (item) {
+            return [item, item];
+          })
+          .reduce(function (a, b) {
+            return a.concat(b);
+          });
+
+        dataCopy.authorized_persons = [];
+        dataCopy.map = false;
+
+        return dataCopy;
+      })()
+    }
+  ]
 };
