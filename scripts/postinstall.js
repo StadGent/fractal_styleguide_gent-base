2fb14679cff55cb527a25c1208ac2bb56cf7bac2
bart <bart.delrue@digipolis.gent>
2018-11-28 15:58:21 +0100

    Fixed codestyle errors.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -9,14 +11,14 @@ var componentsRootPath = path.resolve(__dirname, '../components');
 * Create the necessary directories and move files to them after npm install or yarn install is run.
 */
 if (fs.existsSync(publicRootPath)) {
-    fs.removeSync(`${publicRootPath}/styleguide/vendor`);
+  fs.removeSync(`${publicRootPath}/styleguide/vendor`);
 
-    fs.ensureDirSync(`${publicRootPath}/styleguide/vendor`);
+  fs.ensureDirSync(`${publicRootPath}/styleguide/vendor`);
 
-    fs.copySync(`${nodeModulesRootPath}/baguettebox.js`, `${publicRootPath}/styleguide/vendor/baguettebox`, { recursive: true });
-    fs.copySync(`${nodeModulesRootPath}/masonry-layout`, `${publicRootPath}/styleguide/vendor/masonry-layout`, { recursive: true });
-    fs.copySync(`${nodeModulesRootPath}/imagesloaded`, `${publicRootPath}/styleguide/vendor/imagesloaded`, { recursive: true });
-    fs.copySync(`${nodeModulesRootPath}/allietabs/dist`, `${publicRootPath}/styleguide/vendor/allietabs`, { recursive: true });
+  fs.copySync(`${nodeModulesRootPath}/baguettebox.js`, `${publicRootPath}/styleguide/vendor/baguettebox`, {recursive: true});
+  fs.copySync(`${nodeModulesRootPath}/masonry-layout`, `${publicRootPath}/styleguide/vendor/masonry-layout`, {recursive: true});
+  fs.copySync(`${nodeModulesRootPath}/imagesloaded`, `${publicRootPath}/styleguide/vendor/imagesloaded`, {recursive: true});
+  fs.copySync(`${nodeModulesRootPath}/allietabs/dist`, `${publicRootPath}/styleguide/vendor/allietabs`, {recursive: true});
 }
 
 /*
@@ -25,5 +27,5 @@ if (fs.existsSync(publicRootPath)) {
 * does not exist, so we don't have to create this file here.
 */
 if (fs.existsSync(componentsRootPath)) {
-    fs.copyFileSync(path.resolve(__dirname, 'main_cli.scss'), `${componentsRootPath}/main_cli.scss`);
+  fs.copyFileSync(path.resolve(__dirname, 'main_cli.scss'), `${componentsRootPath}/main_cli.scss`);
 }
