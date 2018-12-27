59ce268bf1daca1c4a95e711ef12fce9fe635edc
Helena Standaert <helena.standaert@gmail.com>
2018-12-27 14:41:50 +0100

    DTGB-656: Removed content from the template and added it as config

--- components/31-molecules/frequently-visited/frequently-visited.config.js
+++ components/31-molecules/frequently-visited/frequently-visited.config.js
@@ -4,6 +4,9 @@ module.exports = {
   title: 'Frequently visited',
   status: 'ready',
   context: {
+    title: 'What are you looking for?',
+    description: 'Enter one or more search terms in the search field above.',
+    subtitle: 'Frequently visited',
     tags: ['Make an appointment', 'Opening hours', 'Events', 'Districts', 'Road works', 'Certificates', 'Job openings']
   }
 };
