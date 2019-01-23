badc24003c0c4b4e9c8acbd79d3039e1dbaa2c14
Helena Standaert <helena.standaert@gmail.com>
2019-01-23 08:40:15 +0100

    DTGB-673: Added Gentinfo component

---
+++ components/31-molecules/gent-info/gent-info.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  status: 'alpha',
+  variants: [
+    {
+      name: 'default',
+      context: {
+        paragraphs: [
+          'Gentinfo is hét centrale aanspreekpunt voor informatie over het stadsbestuur en de stadsdiensten. Heeft u vragen, klachten, suggesties, medlingen,… over de Stad Gent? Neem dan contact op met Gentinfo.',
+          '<strong>Bereikbaar van maandag t.e.m. zaterdag van 8 tot 19 uur.</strong>'
+        ],
+        listItems: [
+          '<i aria-hidden="true" class="icon-phone"></i><a href="tel:092101010">09 210 10 10</a>',
+          '<i aria-hidden="true" class="icon-envelope"></i><a href="mailto:gentinfo@stad.gent">Gentinfo@stad.gent</a>',
+          '<i aria-hidden="true" class="icon-document"></i><a href="#!">Contactformulier</a>'
+        ]
+      }
+    }
+  ]
+};
