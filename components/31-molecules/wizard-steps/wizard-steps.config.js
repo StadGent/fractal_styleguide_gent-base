024088065a679bf4bbcf477030ee1e39e41aa428
bart <bart.delrue@digipolis.gent>
2019-01-29 16:04:01 +0100

    DTBG-677: Added mobile styling.

--- components/31-molecules/wizard-steps/wizard-steps.config.js
+++ components/31-molecules/wizard-steps/wizard-steps.config.js
@@ -9,10 +9,10 @@ module.exports = {
   },
   context: {
     steps: [
-      {text: 'first step', 'url': '#', completed: true},
-      {text: 'second step', 'url': '#', completed: true},
-      {text: 'third step', 'url': '#'},
-      {text: 'fourth step with a long name', 'url': '#'},
+      {text: 'first step', url: '#'},
+      {text: 'second step', url: '#'},
+      {text: 'third step', url: '#'},
+      {text: 'fourth step with a long name', url: '#'},
       {text: 'inactive fifth step'},
       {text: 'inactive sixth step'},
       {text: 'inactive seventh step'},
@@ -23,6 +23,66 @@ module.exports = {
     {
       name: 'default',
       preview: '@preview'
-    }
+    },
+    {
+      name: 'first',
+      preview: '@preview',
+      context: {
+        steps: [
+          {text: 'first step', url: '#'},
+          {text: 'second step', url: '#'},
+          {text: 'third step', url: '#'},
+          {text: 'fourth step with a long name', url: '#'},
+          {text: 'inactive fifth step'},
+          {text: 'inactive sixth step'}
+        ],
+        active: 1
+      }
+    },
+    {
+      name: 'second',
+      preview: '@preview',
+      context: {
+        steps: [
+          {text: 'first step', url: '#'},
+          {text: 'second step', url: '#'},
+          {text: 'third step', url: '#'},
+          {text: 'fourth step with a long name', url: '#'},
+          {text: 'inactive fifth step'},
+          {text: 'inactive sixth step'}
+        ],
+        active: 2
+      }
+    },
+    {
+      name: 'second to last',
+      preview: '@preview',
+      context: {
+        steps: [
+          {text: 'first step', url: '#'},
+          {text: 'second step', url: '#'},
+          {text: 'third step', url: '#'},
+          {text: 'fourth step with a long name', url: '#'},
+          {text: 'inactive fifth step', url: '#'},
+          {text: 'inactive sixth step', url: '#'}
+        ],
+        active: 5
+      }
+    },
+    {
+      name: 'last',
+      preview: '@preview',
+      context: {
+        steps: [
+          {text: 'first step', url: '#'},
+          {text: 'second step', url: '#'},
+          {text: 'third step', url: '#'},
+          {text: 'fourth step with a long name', url: '#'},
+          {text: 'inactive fifth step', url: '#'},
+          {text: 'inactive sixth step', url: '#'}
+        ],
+        active: 6
+      }
+    },
   ]
 };
