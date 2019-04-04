f5942878abb9ae5ff266fcebe0e5a6aa82e8a6f7
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2019-04-04 16:06:46 +0200

    DTGB-536: Added some refactoring.

--- components/21-atoms/table/table-mobile.functions.js
+++ components/21-atoms/table/table-mobile.functions.js
@@ -11,10 +11,13 @@ var MobileTable = function (element) { // eslint-disable-line no-unused-vars
    */
   var caption = null;
 
+  /**
+   * The responsive list version of the table.
+   */
   var tableList = null;
 
   /**
-   * Build
+   * Add a wrapper div with class .table-list as next sibling to the table.
    */
   var addTableList = function () {
     var tableListContainer = document.createElement('div');
@@ -24,33 +27,42 @@ var MobileTable = function (element) { // eslint-disable-line no-unused-vars
     tableList = table.nextElementSibling;
   };
 
+  /**
+   * Adds the individual items to the list <ul></ul> of the table-list wrapper.
+   */
   var addListItems = function (list) {
-    var rowsNodeList = table.querySelector('table').querySelectorAll('tr');
+    // Get the headers of the table columns.
     var headingsNodeList = table.querySelector('table').querySelectorAll('th[scope="col"');
+    // Get all rows of the table.
+    var rowsNodeList = table.querySelector('table').querySelectorAll('tr');
 
+    // Add the rows as a list item.
     for (var i = 0; i < rowsNodeList.length; i++) {
       var row = rowsNodeList[i];
 
-      // Exclude first row as they are headers.
+      // Get all the columns of the table based on the current row.
+      var colsNodeList = row.querySelectorAll('td');
+
+      // Exclude first row as they contain the headers.
       if (i !== 0) {
         var listItem = document.createElement('li');
         list.appendChild(listItem);
 
-        var colsNodeList = row.querySelectorAll('td');
-
+        // Add a header based on the row heading.
         var header = document.createElement('h3');
         header.innerHTML = row.querySelector('th').innerHTML;
         listItem.appendChild(header);
 
+        // Create the definition list containing all columns as dd's.
         var defList = document.createElement('dl');
         listItem.appendChild(defList);
 
         for (var j = 0; j < colsNodeList.length; j++) {
           var column = colsNodeList[j];
 
-          var dt = document.createElement('dt');
-          dt.innerHTML = headingsNodeList[j+1].innerHTML; // Refactor this j + 1 line.
-          defList.appendChild(dt);
+          var term = document.createElement('dt');
+          term .innerHTML = headingsNodeList[j + 1].innerHTML; // Refactor this j + 1 line.
+          defList.appendChild(term);
 
           var item = document.createElement('dd');
           item.innerHTML = column.innerHTML;
@@ -60,6 +72,9 @@ var MobileTable = function (element) { // eslint-disable-line no-unused-vars
     }
   };
 
+  /**
+   * Add an unordered list element to the table-list container.
+   */
   var addList = function () {
     var list = document.createElement('ul');
     tableList.appendChild(list);
--- components/21-atoms/table/table.bindings.js
+++ components/21-atoms/table/table.bindings.js
@@ -5,11 +5,16 @@
 (function () {
   var tablesNodeList = document.querySelectorAll('.responsive-table');
 
+  // Optimise all tables with a wrapper div.responsive-table
   for (var i = 0; i < tablesNodeList.length; i++) {
     var table = tablesNodeList[i];
+
+    // Adds accessibility support.
     new ResponsiveTable(table, { // eslint-disable-line no-undef
       scrollableText: '(scroll to see more)'
     });
+
+    // Adds a mobile solution using definition lists.
     new MobileTable(table); // eslint-disable-line no-undef
   }
 
