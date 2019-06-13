a189792ac26bc61830ad59601c419a5bfa3a5b95
bart <bart.delrue@digipolis.gent>
2019-06-13 11:33:51 +0200

    Removed base.js dependencies (improper usages and issues in modern JS frameworks).

--- components/31-molecules/menu/menu.functions.js
+++ components/31-molecules/menu/menu.functions.js
@@ -14,18 +14,92 @@
     }
   }
 }(this || window, function () {
-  return function (elem, options) {
-    if (typeof gent_styleguide === 'undefined') {
-      console.error('You need to include base.js.'); // eslint-disable-line no-console
-      return;
+
+  /**
+   * Generates a tabTrap object
+   *
+   * @param {object} container DOM-element
+   * @constructor
+   */
+  function TabTrap(container) {
+    let focusables = getFocusables(container);
+
+    /**
+     * Returns all focusable elements within a given container.
+     *
+     * @param {object} container hamburger DOM-element
+     * @return {array} focusable elements
+     */
+    function getFocusables(container) {
+      let focusables = container.querySelectorAll(
+        'a[href], ' +
+        'area[href], ' +
+        'input:not([disabled]):not([hidden]), ' +
+        'select:not([disabled]), ' +
+        'textarea:not([disabled]), ' +
+        'button:not([disabled]), ' +
+        '[tabindex="0"]'
+      );
+      return Array.prototype.slice.call(focusables);
     }
 
+    this.setFocusables = function () {
+      focusables = getFocusables(container);
+    };
+
+    this.next = function (e) {
+      let active = document.activeElement;
+      if (active && active === focusables[focusables.length - 1]) {
+        focusables[0].focus();
+        e.preventDefault();
+      }
+    };
+
+    this.back = function (e) {
+      let active = document.activeElement;
+      if (active && active === focusables[0]) {
+        focusables[focusables.length - 1].focus();
+        e.preventDefault();
+      }
+    };
+
+    this.home = function () {
+      focusables[0].focus();
+    };
+
+    this.end = function () {
+      focusables[focusables.length - 1].focus();
+    };
+
+    this.hasFocusables = focusables && focusables.length > 0;
+  }
+
+  /**
+   * Toggle a scroll lock on a visible parent modal or on the body.
+   *
+   * @param {boolean} release or lock parent.
+   * @param {object} elem DOM element.
+   * @param {string} [parentSelector] Queryselectorstring.
+   */
+  const toggleScrollLockParent = (release, elem, parentSelector) => {
+    parentSelector = parentSelector || '.modal.visible';
+
+    const parentModal = elem.parentNode.closest(parentSelector);
+    if (parentModal) {
+      parentModal.style.overflow = release ? '' : 'hidden';
+    }
+    else {
+      document.body.style.overflow = release ? '' : 'hidden';
+    }
+  };
+
+  return function (elem, options) {
     const openbtn = elem.querySelector('.toggle');
     const closebtn = elem.querySelector('.close');
     const drawer = elem.querySelector('.drawer');
     const overlay = elem.querySelector('.overlay');
 
-    const tabTrap = new gent_styleguide.TabTrap(drawer); // eslint-disable-line no-undef
+    const tabTrap = new TabTrap(drawer); // eslint-disable-line no-undef
 
     /**
      * Closes the hamburger menu
@@ -47,8 +121,7 @@
         openbtn.setAttribute('aria-expanded', false);
       }
 
-      // eslint-disable-next-line no-undef
-      gent_styleguide.toggleScrollLockParent(true, drawer);
+      toggleScrollLockParent(true, drawer);
 
       // remove the menu from the tabindex
       setTimeout(function () {
@@ -84,8 +157,7 @@
       // handle keyboard input
       document.addEventListener('keydown', handleKeyboardInput);
 
-      // eslint-disable-next-line no-undef
-      gent_styleguide.toggleScrollLockParent(false, drawer);
+      toggleScrollLockParent(false, drawer);
     };
 
     /**
