39476b089c18b7934a91fea3f387c3103a2fe5a2
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2019-09-25 08:21:48 +0200

    DTGB-748: Added template fixes.

--- components/31-molecules/partner-block/partner-block.config.js
+++ components/31-molecules/partner-block/partner-block.config.js
@@ -4,7 +4,37 @@ module.exports = {
   status: 'alpha',
   context: {
     title: 'Partners',
-    description: 'Hier komt een begeleidende tekst bij het logo.'
+    description: 'Here comes information about the partners. The logos in this block are not predefined and can be changed per project.',
+    items: [
+      {
+        link: '/styleguide/img/stock/partner1.svg',
+        alt_text: 'University of Ghent'
+      },
+      {
+        link: '/styleguide/img/stock/partner2.svg',
+        alt_text: 'HoGent'
+      },
+      {
+        link: '/styleguide/img/stock/partner3.svg',
+        alt_text: 'Artevelde Hogeschool'
+      },
+      {
+        link: '/styleguide/img/stock/partner4.svg',
+        alt_text: 'Odisee'
+      },
+      {
+        link: '/styleguide/img/stock/partner5.svg',
+        alt_text: 'KU Leuven'
+      },
+      {
+        link: '/styleguide/img/stock/partner6.svg',
+        alt_text: 'Luca'
+      },
+      {
+        link: '/styleguide/img/stock/partner7.svg',
+        alt_text: 'Student in Ghent'
+      }
+    ]
   },
   variants: [
     {
@@ -12,7 +42,7 @@ module.exports = {
       context: {
         title: '',
         single_partner: true,
-        description: 'Hier komt een begeleidende tekst bij het logo die kort meer uitleg geeft.'
+        description: 'Here comes information about the partners. The logos in this block are not predefined and can be changed per project.'
       }
     }
   ]
