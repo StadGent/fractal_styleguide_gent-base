1d433894679cce7789756bde89367a5f1aa60c10
Helena Standaert <helena.standaert@gmail.com>
2019-10-01 08:24:52 +0200

    DTGB-689: Replace box occurrences with highlight

--- components/31-molecules/highlight/highlight.config.js
+++ components/31-molecules/highlight/highlight.config.js
@@ -2,21 +2,26 @@
 
 module.exports = {
   status: 'alpha',
+  default: 'image-left',
   context: {
     content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum imperdiet vestibulum ex, id tincidunt nulla porttitor nec.</p>'
   },
   variants: [
     {
-      name: 'spot-image-left',
+      name: 'image-left',
       context: {
         modifier: 'left'
       }
     },
     {
-      name: 'spot-image-top',
+      name: 'image-top',
       context: {
         modifier: 'top'
       }
+    },
+    {
+      name: 'no-image',
+      hidden: true
     }
   ]
 };
