9fe280cba685ba669b83972f829c50ac060ea4c1
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2019-10-04 12:19:18 +0200

    DTGB-748: Added links to partner block logos.

--- components/31-molecules/partner-block/partner-block.config.js
+++ components/31-molecules/partner-block/partner-block.config.js
@@ -8,31 +8,38 @@ module.exports = {
     items: [
       {
         link: '/styleguide/img/stock/partner1.svg',
-        alt_text: 'University of Ghent'
+        alt_text: 'University of Ghent',
+        link_title: 'Link to the University of Ghent website'
       },
       {
         link: '/styleguide/img/stock/partner2.svg',
-        alt_text: 'HoGent'
+        alt_text: 'HoGent',
+        link_title: 'Link to the HoGent website'
       },
       {
         link: '/styleguide/img/stock/partner3.svg',
-        alt_text: 'Artevelde Hogeschool'
+        alt_text: 'Artevelde Hogeschool',
+        link_text: 'Link to the Artevelde Hogeschool website'
       },
       {
         link: '/styleguide/img/stock/partner4.svg',
-        alt_text: 'Odisee'
+        alt_text: 'Odisee',
+        link_text: 'Link to the Odisee website'
       },
       {
         link: '/styleguide/img/stock/partner5.svg',
-        alt_text: 'KU Leuven'
+        alt_text: 'KU Leuven',
+        link_text: 'Link to the KU Leuven website'
       },
       {
         link: '/styleguide/img/stock/partner6.svg',
-        alt_text: 'Luca'
+        alt_text: 'Luca',
+        link_text: 'Link to the Luca website'
       },
       {
         link: '/styleguide/img/stock/partner7.svg',
-        alt_text: 'Student in Ghent'
+        alt_text: 'Student in Ghent',
+        link_text: 'Link to the Student in Ghent website'
       }
     ]
   },
