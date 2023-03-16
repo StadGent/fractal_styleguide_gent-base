9a9c7c7cde2ef088bb74b4b4c0029ddf4a33257e
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-03-16 15:45:04 +0100

    SGD8-2488: Update status message styling

--- components/31-molecules/status-message/status-message.config.js
+++ components/31-molecules/status-message/status-message.config.js
@@ -3,8 +3,8 @@
 module.exports = {
   context: {
     text: 'Aliquam ullamcorper vehicula leo. Mauris mattis egestas hendrerit. Sed <a href="#">vitae aliquet mauris</a>. Morbi at erat sed mi mollis maximus at sit amet enim. Sed quis libero eu mauris eleifend accumsan vitae semper turpis. Vestibulum nisl ipsum, ullamcorper euismod ultrices nec, tempor sit amet urna. Pellentesque bibendum a mi commodo consectetur. Aliquam nec est tellus.',
-    modifier: 'messages--info',
-    icon: 'icon-info'
+    icon: 'icon-info-circle',
+    modifier: 'messages--info'
   },
   variants: [
     {
@@ -14,21 +14,21 @@ module.exports = {
     {
       name: 'warning',
       context: {
-        icon: 'icon-exclamation',
+        icon: 'icon-exclamation-circle',
         modifier: 'messages--warning'
       }
     },
     {
       name: 'error',
       context: {
-        icon: 'icon-cross',
+        icon: 'icon-cross-circle',
         modifier: 'messages--error'
       }
     },
     {
       name: 'success',
       context: {
-        icon: 'icon-checkmark',
+        icon: 'icon-checkmark-circle',
         modifier: 'messages--status'
       }
     }
