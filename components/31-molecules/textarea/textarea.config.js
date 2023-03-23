683a01cfd35eda6df43935abef911d6bbf801e4e
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-03-23 10:46:49 +0100

    SGD8-2482: Update textarea styling

--- components/31-molecules/textarea/textarea.config.js
+++ components/31-molecules/textarea/textarea.config.js
@@ -6,23 +6,31 @@ module.exports = {
     input_component: 'input-textarea',
     label: 'Textarea'
   },
-  variants: [{
-    name: 'with-error',
-    context: {
-      id: 'textarea--error',
-      modifier: 'error',
-      field_description: 'You can add an optional field description here.',
-      field_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet, urna sit amet convallis rhoncus, felis ex.'
+  variants: [
+    {
+      name: 'with error',
+      context: {
+        id: 'textarea--error',
+        modifier: 'error',
+        field_description: 'You can add an optional field description here.',
+        field_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet, urna sit amet convallis rhoncus, felis ex.'
+      }
+    },
+    {
+      name: 'with success',
+      context: {
+        id: 'textarea--success',
+        modifier: 'success',
+        field_description: 'You can add an optional field description here.',
+        field_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet, urna sit amet convallis rhoncus, felis ex.'
+      }
+    },
+    {
+      name: 'disabled',
+      context: {
+        id: 'textarea--disabled',
+        disabled: true
+      }
     }
-  },
-  {
-    name: 'with-success',
-    context: {
-      id: 'textarea--success',
-      modifier: 'success',
-      field_description: 'You can add an optional field description here.',
-      field_message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet, urna sit amet convallis rhoncus, felis ex.'
-    }
-  }
   ]
 };
