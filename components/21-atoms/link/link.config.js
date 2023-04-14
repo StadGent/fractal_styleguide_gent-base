464d31a774b03fae01f93b40d0a2a42d4b671e6d
De Tant Fabio <fabio.detant@district09.gent>
2023-04-14 11:58:02 +0200

    SGD8-2466: Alter the link template to use new variables and add variants to js config

--- components/21-atoms/link/link.config.js
+++ components/21-atoms/link/link.config.js
@@ -65,6 +65,77 @@ module.exports = {
       }
     },
     {
+      name: 'external-link-background-dark',
+      context: {
+        link: 'https://stad.gent',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'download-link-background-dark',
+      context: {
+        link: '#',
+        download: true,
+        background: 'dark-background'
+      }
+    },
+    {
+      name: 'mailto-link-background-dark',
+      context: {
+        link: 'mailto:info@stad.gent',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'standalone-background-dark',
+      context: {
+        link: '#',
+        modifier: 'standalone-link',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'standalone-external-background-dark',
+      context: {
+        link: 'https://stad.gent',
+        modifier: 'standalone-link',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'standalone-mailto-background-dark',
+      context: {
+        link: 'mailto:info@stad.gent',
+        modifier: 'standalone-link',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'standalone-download-background-dark',
+      context: {
+        link: '#',
+        modifier: 'standalone-link',
+        download: true,
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
+      name: 'standalone-back-background-dark',
+      context: {
+        link: '#',
+        text: 'back',
+        modifier: 'standalone-link back',
+        background: 'dark-background',
+        isExample: true
+      }
+    },
+    {
       name: 'button',
       context: {
         link: '#',
