1b4d5ddd637d6ee5a18e53bbdddd62b63953a934
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-04-24 16:34:31 +0200

    SGD8-2506: Update themefooter styling

--- components/31-molecules/feature-block/feature-block.config.js
+++ components/31-molecules/feature-block/feature-block.config.js
@@ -6,6 +6,7 @@ module.exports = {
   context: {
     modifier: 'primary dark-background',
     title: 'This is the title',
+    heading_type: 'h1',
     content: 'Any variety of content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
   },
   variants: [
