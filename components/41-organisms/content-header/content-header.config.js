5523189c403584ffac2eb1ec90cb59b3750689f5
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-06-05 15:33:56 +0200

    SGD8-2519, SGD8-2518, SGD8-2517, SGD8-2516, SGD8-2515: Add content headers

---
+++ components/41-organisms/content-header/content-header.config.js
@@ -0,0 +1,89 @@
+'use strict';
+
+module.exports = {
+  preview: '@preview-description-list-without-padding',
+  collator: function (markup, item) {
+    return `<!-- Start: @${item.handle} -->\n<dt>${item.name}</dt><dd style="max-width: 100%; padding: 0;">${markup}</dd>\n<!-- End: @${item.handle} -->\n`;
+  },
+  default: 'Title boxed',
+  context: {
+    modifier: 'primary dark-background',
+    speech_button: true,
+    summary: false,
+    summary_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa.',
+    popular_topics: false,
+    image: false
+  },
+  variants: [
+    {
+      name: 'Title boxed with metadata in primary color',
+      context: {
+        modifier: 'primary dark-background',
+        summary: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata in primary color with image',
+      context: {
+        modifier: 'primary dark-background',
+        summary: true,
+        summary_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit ipsum sed gravida posuere. Maecenas sit amet sapien aliquam, semper elit in, sodales massa.',
+        image: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata in secondary color with image',
+      context: {
+        modifier: 'secondary',
+        summary: true,
+        image: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata in secondary color with image and event summary',
+      context: {
+        modifier: 'secondary',
+        summary: true,
+        event_summary: true,
+        image: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata in secondary color without image',
+      context: {
+        modifier: 'secondary',
+        summary: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata in secondary color with event summary without image',
+      context: {
+        modifier: 'secondary',
+        summary: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata and accolade design with image',
+      context: {
+        modifier: 'accolade-title',
+        popular_topics: true,
+        image: true
+      }
+    },
+    {
+      name: 'Title boxed with metadata and accolade design without image',
+      context: {
+        modifier: 'accolade-title',
+        popular_topics: true
+      }
+    },
+    {
+      name: 'Title boxed without metadata and accolade design without image',
+      context: {
+        modifier: 'accolade-title',
+        popular_topics: false,
+        image: false
+      }
+    }
+  ]
+};
