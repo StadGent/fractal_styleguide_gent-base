62aa4ffe4ad46bcfd1393088b84ca6388832e190
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-06-12 16:47:16 +0200

    SGD8-2461: Add all colors as base colors

--- components/11-base/colors/colors.config.js
+++ components/11-base/colors/colors.config.js
@@ -4,29 +4,30 @@ module.exports = {
   context: {
     primary: {
       'blue': '#005ba9',
+      'blue-light': '#ccdeee',
       'yellow': '#ffdb5a',
+      'yellow-pastel:': '#ffec93',
+      'yellow-light': '#fff8d4',
       'cyan': '#009de0',
-      'cyan-hover': '#1abcff',
-      'white': '#ffffff'
+      'cyan-pastel': '#7ed9ff',
+      'cyan-light': '#e6f8ff',
+      'cyan-hover': '#1abcff'
     },
     secondary: {
+      'white': '#ffffff',
       'gray': '#001823',
+      'gray-medium': '#687278',
+      'gray-light': '#e6e8ea',
+      'gray-ultra-light': '#f0f1f2',
       'red': '#ea0d33',
+      'red-pastel': '#ff8a9d',
+      'red-light': '#ffe8ec',
       'orange': '#e06c00',
+      'orange-pastel': '#ffb673',
+      'orange-light': '#fff2e5',
       'green': '#2c8726',
-      'green-pastel': '#8ee088'
-    },
-    tints: {
-      '1': '10%',
-      '2': '25%',
-      '3': '50%',
-      '-1': '75%',
-      '-2': '60%',
-      '-3': '50%',
-      '-4': '25%',
-      '-5': '10%',
-      '-6': '6%',
-      '-7': '2%'
+      'green-pastel': '#8ee088',
+      'green-light': '#e1fde1'
     }
   }
 };
