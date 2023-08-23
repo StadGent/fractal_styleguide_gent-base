81a5836dbb1ecc06eb5c745dba67d0dea9d8c0a1
Lennart Van Vaerenbergh <lennart_v_v@hotmail.com>
2023-08-23 10:34:28 +0200

    SGD8-2488: Fix icon size of status messages

--- components/31-molecules/table-of-contents/table-of-contents.config.js
+++ components/31-molecules/table-of-contents/table-of-contents.config.js
@@ -5,8 +5,8 @@ module.exports = {
   context: {
     list: [
       '<a href="#" class="active">Anchor link 1</a>',
-      '<a href="#">Anchor link 2</a>',
-      '<a href="#">Anchor link 3</a>',
+      '<a href="#">Anchor link 2 that is slightly longer</a>',
+      '<a href="#">Anchor link 3 that is also a bit longer to see the behaviour</a>',
       '<a href="#">Anchor link 4</a>',
       '<a href="#">Anchor link 5</a>',
       '<a href="#">Anchor link 6</a>',
