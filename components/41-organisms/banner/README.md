932e3808d4e6d0e7ac9ccf577e51cba225164766
SvenVdv <sven.vandevenne@gmail.com>
2024-01-18 12:41:20 +0100

    SGD8-2596: Remove multiple images

--- components/41-organisms/banner/README.md
+++ components/41-organisms/banner/README.md
@@ -10,7 +10,7 @@ For images, the following requirements should be followed:
 
 * An alt attribute is required. A short text alternative should be specified using the alt attribute. [See WCAG Technique page](https://www.w3.org/TR/WCAG20-TECHS/H37.html)
 * For images that are purely decorative images, the alt attributes must be empty.
-* An aspect ratio should always be provided. The default aspect ratio is 8:5.
+* The aspect ratio is determined by the image inside
 * When the image cannot be loaded or is not found, a placeholder is used. See the "broken image placeholder" example.
 * When there is no image to show, a placeholder is used. See the "no image placeholder" example.
 
--- components/41-organisms/banner/banner.config.js
+++ components/41-organisms/banner/banner.config.js
@@ -11,30 +11,5 @@ module.exports = {
         caption: 'image caption'
       }
     ]
-  },
-  variants: [
-    {
-      name: 'Multiple images',
-      context: {
-        multiple: true,
-        images: [
-          {
-            src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
-            alt: 'placeholder image alternative text',
-            caption: 'image caption'
-          },
-          {
-            src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
-            alt: 'placeholder image alternative text',
-            caption: 'image caption'
-          },
-          {
-            src: 'https://via.placeholder.com/800x500&text=8:5+(800x500)',
-            alt: 'placeholder image alternative text',
-            caption: 'image caption'
-          }
-        ]
-      }
-    }
-  ]
+  }
 };
